0e50a9b745d57e90323cf1a56de519c923cb1aea
Jason Wang <jasonwang332@gmail.com>
2025-06-21 16:58:19 +1200

    display file name and size

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import "./App.css"
 
 function App() {
-  const handleDrop = (e) => {
-    e.preventDefault();
-    console.log("Something was dropped!");
+  const [fileName, setFileName] = useState("")
+  const [fileSize, setFileSize] = useState("")
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    setFileName(event.dataTransfer.files[0].name)
+    setFileSize(event.dataTransfer.files[0].size / 1000)
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
+  const handleDragOver = (event) => {
+    event.preventDefault();
   };
 
   return (
-    <div
-      onDrop={handleDrop}
-      onDragOver={handleDragOver}
-      style={{
-        width: "100%",
-        height: "100vh",
-        border: "2px dashed #ccc",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h2>Drop folder here</h2>
-    </div>
+    <>
+      <div
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        style={{
+          width: "100%",
+          height: "100vh",
+          border: "2px dashed #ccc",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2>Drop folder here</h2>
+      </div>
+      <div className="file-container">
+        <p className="file-name">{fileName}</p>
+        <p className="file-size"> {fileSize} kb</p>
+      </div>
+    </>
   );
 }
 
